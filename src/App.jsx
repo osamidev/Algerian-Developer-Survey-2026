f76e90aa9bcad535ad5e765cc3cc5508ab11d77c
akram <yaciaakram@gmail.com>
2026-03-18 17:44:16 +0100

    create the survey page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import LandingPage from './pages/LandingPage';
+import Survey from './pages/Survey';
 
 
 
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        
+        <Route path="/" element={<LandingPage />} />     
       </Routes>
       
     
