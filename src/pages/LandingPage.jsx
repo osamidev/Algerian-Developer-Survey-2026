09ccda32e6f42143ac9c69edebf09dcacca92ec8
Younes <oussamatrix96@gmail.com>
2026-04-20 14:13:39 +0100

    feat: add landing, survey, and auth pages

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
-function LandingPage(){
-    return(
-        <div>
-            hello this is Landing page
-        </div>
-    )
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function LandingPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/survey");
+  });
+  return <div>hello this is Landing page</div>;
 }
 
-export default LandingPage
+export default LandingPage;
--- src/pages/SurveyPage.jsx
+++ src/pages/SurveyPage.jsx
@@ -1,5 +1,18 @@
+import { useForm, FormProvider } from "react-hook-form";
+import { QuestionsProvider } from "../Contexts/QuestionsContext";
+import mockData from "../assets/mockData.json";
+import SurveyShell from "../features/survey/SurveyShell";
+
 function SurveyPage() {
-  return <div></div>;
+  const methods = useForm({ mode: "onChange" });
+
+  return (
+    <FormProvider {...methods}>
+      <QuestionsProvider questions={mockData.questions}>
+        <SurveyShell />
+      </QuestionsProvider>
+    </FormProvider>
+  );
 }
 
 export default SurveyPage;
